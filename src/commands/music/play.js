47b3c99b67ec5da36be17b40e06dd29fbd203875
Álex S. R <85245377+alexdiple@users.noreply.github.com>
2022-04-11 18:02:36 +0200

    Refactored to clean code

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,104 +1,67 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const wait = require('node:timers/promises').setTimeout;
 const { QueryType } = require('discord-player');
-const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 
-    /**
-     * Command Propieties
-     */
+    // Slash Command Propeties
     data: new SlashCommandBuilder()
-    .setName('play')
-    .setDescription('Plays the song you want')
-    .addStringOption(option => 
-        option.setName('song')
-            .setDescription('Say the song you want to play')
-            .setRequired(true)),
+        .setName('play')
+        .setDescription('Plays the song you want')
+        .addStringOption(option => 
+            option.setName('song')
+                .setDescription('Say the song you want to play')
+                .setRequired(true)),
 
     /**
-     * Command Action to execute.
+     * Command Action.
      * 
-     * @param {*} interaction 
+     * @param {*} interaction is the <CommandInteraction> created from Command.
      */
     async execute(interaction){
-        // console.log(interaction)
-        /**
-         * Set 'Bot is thinking...'
-         */
-        await interaction.deferReply()
-        await wait(2000);
 
-        /**
-         * Util Constants.
-         */
+        // Defer the reply (Send reply with 'Bot is thinking...')
+        await interaction.deferReply();
+        await wait(1000);
 
-         const guildId = interaction.guildId;
-         const channelId = interaction.channelId;
- 
-         /**
-          * Command arguments.
-          */
- 
-         const song = interaction.options.getString('song');
- 
-         /**
-          * Fetch the song.
-          * 
-          * Busca la canción utilizando el método 'search' de discord-player.
-          * 
-          * El método 'search' admite más opciones en el segundo parámetro.
-          * Search Method Doc: https://discord-player.js.org/docs/main/master/class/Player?scrollTo=search
-          * 
-          * @returns Promise<PlayerSearchResult>
-          */
- 
-         const res = await global.player.search(song, {
-             requestedBy: interaction.user,
-             searchEngine: QueryType.AUTO
-         });
- 
-         /**
-          * Create a Queue.
-          * 
-          * Creates a queue for a guild if not available, else returns existing queue
-          * 
-          * https://discord-player.js.org/docs/main/master/class/Player?scrollTo=createQueue
-          */
- 
-         const queue = await global.player.createQueue(guildId, {
-             metadata: channelId
-         });
+        // Util Data.
+        const guildId = interaction.guildId;
+        const channelId = interaction.channelId;
 
-         /**
-          * Connect to Voice channel.
-          */
-         try {
-             if (!queue.connection) await queue.connect("959924006033690658");
-         } catch {
-             await global.player.deleteQueue(guildId);
-             return interaction.reply('No se puede acceder al canal de voz.');
-         }
+        // String Query.
+        const song = interaction.options.getString('song');
  
-        /**
-         * Play the song.
-         */ 
-        res.playlist ? queue.addTracks(res.tracks) : queue.addTrack(res.tracks[0]);
+        // Fetch the song.
+        const query = await global.player.search(song, {
+            requestedBy: interaction.user,
+            searchEngine: QueryType.AUTO
+        });
+
+        // Create a Queue.
+        const queue = await global.player.createQueue(guildId, {
+            metadata: channelId
+        });
+
+        // Connect to voice channel. If user is not connected to anyone, send a reply. 
+        try {
+            if (!queue.connection) await queue.connect(interaction.member.voice.channel);
+        } catch {
+            await global.player.deleteQueue(guildId);
+            return interaction.reply({
+                content: `Hey \`@${interaction.user.username}\`, you must join a voice channel to play music! :3`, 
+                ephemeral: true,
+            });
+        }
+ 
+        // Check if result is playlist or song and add it to queue.
+        query.playlist ? queue.addTracks(query.tracks) : queue.addTrack(query.tracks[0]);
+
+        // Play the queue.
         if (!queue.playing) await queue.play();
 
-        /**
-         * Embed Message to reply.
-         */
-        const embed = new MessageEmbed()
-            .setColor('GREEN')
-            .setTitle('▶️ Playing...')
-            .setDescription(queue.current.title)
-            .setThumbnail(queue.current.thumbnail)
-            .setFooter(`[${queue.current.duration}]`)
-
-        /**
-         * Reply.
-         */
-        await interaction.editReply({ embeds: [embed]})
+        // Send reply to text channel.
+        await interaction.editReply({ 
+            content: `**${queue.current.title}** added to queue.`
+         });
     }
 }
